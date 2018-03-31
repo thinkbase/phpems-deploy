8b4523f8281e39c5ea8240c468452805c7761992
thinkbase <thinkbase@users.noreply.github.com>
2018-03-31 23:59:05 +0800

    Run testing env of PHPEMS successful

--- phpems/phpems/lib/config.inc.php
+++ phpems/phpems/lib/config.inc.php
@@ -15,10 +15,10 @@ define('WP','http://'.$_SERVER['SERVER_NAME'].dirname($_SERVER['SCRIPT_NAME']));
 else
 define('WP','http://'.$_SERVER['SERVER_NAME'].'/');
 
-define('DB','777');//MYSQL数据库名
-define('DH','127.0.0.1');//MYSQL主机名，不用改
-define('DU','root');//MYSQL数据库用户名
-define('DP','root');//MYSQL数据库用户密码
+define('DB',getenv('PHPEMS_DB_NAME'));//MYSQL数据库名
+define('DH',getenv('PHPEMS_DB_HOST'));//MYSQL主机名，不用改
+define('DU',getenv('PHPEMS_DB_USER'));//MYSQL数据库用户名
+define('DP',getenv('PHPEMS_DB_PASS'));//MYSQL数据库用户密码
 define('DTH','x2_');//系统表前缀，不用改
 
 /**
